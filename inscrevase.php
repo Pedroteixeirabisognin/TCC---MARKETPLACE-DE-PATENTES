2b6cf2ecca7d15c0da4274ff754c1abb22992eb2
Pedro Teixeira <pedroteixeirabisognin@gmail.com>
2017-10-28 16:34:25 -0200

    Validando o cadastro dos usuários, verificando se não já existe algum campo

--- inscrevase.php
+++ inscrevase.php
@@ -1,3 +1,10 @@
+<?php
+	//ISSET VERIFICA SE EXISTE ALGO DENTRO DA VARIÁVEL
+	$erro_usuario = isset($_GET['erro_usuario'])? $_GET['erro_usuario'] : 0;
+	$erro_email	=  isset($_GET['erro_email'])? $_GET['erro_email'] : 0;
+?>
+
+
 <!DOCTYPE HTML>
 <html lang="pt-br">
 	<head>
@@ -50,10 +57,28 @@
 				<form method="post" action="php/registra_usuario.php" id="formCadastrarse">
 					<div class="form-group">
 						<input type="text" class="form-control" id="usuario" name="usuario" placeholder="Usuário" required="requiored">
+						<?php
+
+							if ($erro_usuario) {//php já reconhece 0 como false e 1 como true
+								echo "<font style='color: #FF0000'>Usuário já existe</font>";
+							}
+
+
+
+						?>
 					</div>
 
 					<div class="form-group">
 						<input type="email" class="form-control" id="email" name="email" placeholder="Email" required="requiored">
+						<?php
+
+							if ($erro_email) {
+								echo "<font style='color: #FF0000'>E-mail já existe</font>";
+							}
+
+
+
+						?>					
 					</div>
 					
 					<div class="form-group">
--- php/registra_usuario.php
+++ php/registra_usuario.php
@@ -12,6 +12,72 @@
 
 	 $link = $objDb->conecta_mysql();
 
+	 $usuario_existe = false;
+	 $email_existe = false;
+	 // VERIFICAR SE O USUÁRIO JÁ EXISTE
+	 $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
+
+	if($resultado_id = mysqli_query($link,$sql)){
+
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+
+		if(isset($dados_usuario['usuario'])){
+
+			$usuario_existe = true; 
+
+		}
+
+	}else{
+
+		echo "Erro ao tentar localizar o registro de usuário";
+
+
+	}
+
+
+
+	 //VERIFICAR SE O E-MAIL JÁ EXISTE
+	if($resultado_id = mysqli_query($link,$sql)){
+
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+
+		if(isset($dados_usuario['email'])){
+
+			$email_existe = true; 
+
+		}
+
+	}else{
+
+		echo "Erro ao tentar localizar o registro de email";
+
+	
+	}
+
+	if ($usuario_existe || $email_existe ) {
+
+		$retorno_get = '';
+
+		if($usuario_existe){
+
+			$retorno_get.= "erro_usuario=1&";
+
+		}
+		if($usuario_existe){
+
+			$retorno_get.= "erro_email=1&";
+
+		}
+		//A INTERROGAÇÃO DEFINE O QUE É SCRIPT E O QUE É PARAMETRO
+		header('Location: ../inscrevase.php?'.$retorno_get);
+		
+		//INTERROMPE A EXECUÇÃO DO SCRIPT
+		die();
+	}
+
+
+
+
 	 // QUERY SQL (NOTA: QUANDO SE UTILIZA ASPAS DUPLAS " O PHP JÁ TENTA ENCONTRAR ALGUMA VARIÁVEL NO MEIO E TENTA ATRIBUIR O VALOR REFERENTE A ELA AO EXECUTAR A STRING)
 	 $sql = "INSERT INTO usuarios(usuario, email, senha) VALUES ('$usuario','$email','$senha')";
 
